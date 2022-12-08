c97cfe6773b0c5fcbd2ba70470b87f37db954546
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-08 15:58:50 -0800

    my 6th commit task on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,13 +12,11 @@ char a;
 int b;
 long int c;
 long long int d;
-float e;
-
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu.\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu.\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu.\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu.\n", (unsigned long)sizeof(e));
+float e
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
-
-};
+}
