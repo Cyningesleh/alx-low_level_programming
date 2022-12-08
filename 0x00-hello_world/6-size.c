c1b774bd81a8ed4620c0110854057b9893029f64
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-08 15:43:50 -0800

    my 6th commit task on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 /**
-* main -Entry point
+* main - Entry point
 *
 * return: Always 0 (success)
 */
 int main(void)
 {
-printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(int));
-printf("%lu\n", sizeof(long int));
-printf("%lu", sizeof(long long int));
-printf("%lu", sizeof(float));
- return (0);
-}
+
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: %lu.\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu.\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu.\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu.\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu.\n", (unsigned long)sizeof(e));
+return (0);
+
+};
