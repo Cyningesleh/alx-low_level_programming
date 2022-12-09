4cc4b3ffc5a9ddd8a3b18aaaa70ad9d37aec93a8
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-08 17:52:55 -0800

    my 8th comit on c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,15 @@
  *main - Entry point
  *
  *
- *Description - Write a C program that prints to stdrr
- *Return : Always 1 (error)
+ *Description - Write a C program that prints to stderr
+ *Return: Always 1 (error)
  */
 int main(void)
 
 {
+
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 59, 1, stderr);
 return (1);
+
 }
