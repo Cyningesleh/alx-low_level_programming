a6ff759b4ee69f6eed5e6c0b7a129de3e2a46369
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-08 17:42:12 -0800

    my 8th comit on c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,10 @@
  *Description - Write a C program that prints to stdrr
  *Return : Always 1 (error)
  */
-
 int main(void)
 
 {
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-       59, 1, stderr);
+59, 1, stderr);
 return (1);
 }
