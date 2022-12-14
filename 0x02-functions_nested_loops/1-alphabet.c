aab0e5a572d7ad0dc066a64549d046220db6f326
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-13 16:28:57 -0800

    my 1st commit on functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-*main - entry point
-*
-*description - To print alphabets in lower case
+*print_alphabet - To print alphabets in lower case
 *
 *Return: Always 0 - (success)
 */
-int main(void)
+void print_alphabet(void)
 {
 int x = 'a';
 while (x <= 'z')
