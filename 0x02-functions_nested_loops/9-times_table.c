117b9557bf12d8811bd7c958d951756350118132
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2022-12-13 20:50:20 -0800

    my 9th commit on functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,8 +10,6 @@ void times_table(void)
 
 int num, mult, prod;
 
-
-
 for (num = 0; num <= 9; num++)
 
 {
@@ -27,10 +25,7 @@ _putchar(',');
 
 _putchar(' ');
 
-
-
 prod = num * mult;
-
 
 if (prod <= 9)
 
@@ -39,8 +34,6 @@ _putchar(' ');
 else
 
 _putchar((prod / 10) + '0');
-
-
 
 _putchar((prod % 10) + '0');
 
