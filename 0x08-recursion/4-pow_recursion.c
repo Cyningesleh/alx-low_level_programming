22edbd582a98ad0501622d3f2ef95ee868df58d7
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-06 12:31:20 -0800

    my 4th commit on recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_pow_recursion - function that prints an integer to the power of y.
+ *
+ *@x: integer to be printed
+ *@y: power of
+ *Return: Always 1.
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+return (1);
+if (y < 0)
+return (-1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
