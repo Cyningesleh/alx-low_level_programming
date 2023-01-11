6ed29fb767f97e8fff6f7cc9243aee6d0e027191
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-11 08:58:14 -0800

    updated task on malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,12 +7,20 @@
  *@c: character string
  *Return: pointer
  */
-
 char *create_array(unsigned int size, char c)
 {
-char *array;
+if (size == 0)
+{
+return (NULL);
+}
+
+char *array = (char *)malloc(sizeof(char) * size);
+if (array == NULL)
+{
+return (NULL);
+}
+
 unsigned int i;
-array =  malloc(size * sizeof(char));
 for (i = 0; i < size; i++)
 {
 array[i] = c;
