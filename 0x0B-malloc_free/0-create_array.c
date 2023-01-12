99be6016226ed79acadaa57fe1f214fe39998aa1
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-11 18:15:08 -0800

    updated commit on malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,18 +9,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-if (size == 0)
-{
-return (NULL);
-}
-
+unsigned int i;
 char *array;
+
+if (size == 0)
+return (NULL);
+
 array = malloc(size * sizeof(char));
 if (array == NULL)
 {
 return (NULL);
 }
-unsigned int i;
 for (i = 0; i < size; i++)
 {
 array[i] = c;
