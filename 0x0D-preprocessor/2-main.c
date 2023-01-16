857d3ccd72b697926a77513fa18a62980f0f5f30
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-16 15:13:21 -0800

    2 task on preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include "stdlib.h"
+#include "stdio.h"
+#define _2_MAIN_C_
+#ifndef _2_MAIN_C_
+#define _PUTCHAR_
+/**
+ *main - Entry point
+ *Description - function that prints the name of the file
+ *Return: 0
+ */
+int main (void)
+{
+_putchar("%s\n", _2_MAIN_c_)
+return (0);
+}
+
+#endif
