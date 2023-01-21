6cb9e79636e7aaa5a1968db0dbd1e6b1e1fc79a0
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-21 03:35:04 -0800

    my updated commit on function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - To print name
@@ -10,5 +10,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 if (*f != NULL)
-f(*name);
+f(name);
 }
