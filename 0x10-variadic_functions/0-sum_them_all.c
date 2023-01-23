b27b1ea3660d2da3422ab33c45d534af8a2a5ee7
Cyningesleh <ifeanyiogbonnaya.jnr@yahoo.com>
2023-01-23 13:21:25 -0800

    updated commit on variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,18 @@
 #include <stdarg.h>
 /**
- *int sum_them_all - sums up integers using variadic function
+ *sum_them_all - sums up integers using variadic function
  *@n: fixed parameter
  *Return: sum or (0) if n == 0;
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int i, sum = 0;
 va_list num;
-if (n == 0);
+if (n == 0)
 return (0);
 va_start(num, n);
-unsigned int i, sum = 0;
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(num, int);
 va_end(num);
 return (sum);
