5c091fc26a1f2d14755458449ee36cd303190d6f
cyningesleh <Ifeanyiogbonnaya.jnr@yahoo.com>
2023-11-29 18:57:28 +0100

    commmit on bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - This function converts a binary numbers to an unsigned int.
+ * @b: a char pointer to a string that contains a binary number
+ * Return: unsigned int with decimal value of binary number, || 0 if error
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int num;
+
+num 0;
+if (b == NULL)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+num << = 1;
+if (b[i] == '1')
+num += 1;
+}
+return (num);
